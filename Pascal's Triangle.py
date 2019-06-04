b9266badb44563d73caae801cc40fdd93b9a5895
U-Sharma <50569208+U-Sharma@users.noreply.github.com>
2019-06-04 19:05:22 +0530

    Made the code even more confusing

--- Pascal's Triangle.py
+++ Pascal's Triangle.py
@@ -2,17 +2,10 @@ def print_pascal_triangle(n):
     if n <= 0:
         return None
     elif n == 1:
-        print("1")
-        return None
-    i = 1
-    l = []
-    t = []
-    
+        return print("1")
+    i, l, t = 1, [], []
     while i <= n:
-        c = l[:]
-        l += [1]
-        d = 0
-        
+        c, l, d = l[:], l+[1], 0
         while d < len(l):
             if d == 0:
                 a = 0
@@ -27,27 +20,18 @@ def print_pascal_triangle(n):
                 else:
                     a = c[d - 1]
             else:
-                a = c[d - 1]
-                b = c[d]
+                a, b = c[d - 1], c[d]
             l[d] = a + b
             d += 1
-        
-        t.append(list(map(str,l)))
-        i += 1
-
-    maxi = max(list(map(len,t[n-1])))
-    i = 0
+        t, i = t + [list(map(str,l))], i + 1
+    maxi, i = max(list(map(len,t[n-1]))), 0
     while i < n:
-        
         d = 0
         while d < len(t[i]):
             t[i][d] += (maxi - len(t[i][d])) * " "
             d += 1
-        
         t[i] = " ".join(t[i])
         t[i] = ((n - i - 1) + (maxi // 2) * (n - i - 1)) * " " + t[i]
         print(t[i])
         i += 1
-        
 print_pascal_triangle(int(input("Please enter the size of the Pascal's triangle: ")))
-# A program to print a pascal's triangle with the size entered
